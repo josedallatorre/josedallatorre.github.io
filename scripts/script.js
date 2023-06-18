f58f84f1d1b9d0f081d5db7a12b9ad57ebc26c7c
josedallatorre <jose.dallatorre@hotmail.com>
2023-06-19 00:32:25 +0200

    feat: added img for repo and dynamic retrieve repo

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,32 @@ async function updateRepos() {
       //console.log(data);
       
       // Loop over each object in data array
+      /*<div class="bgrid folio-item">
+	               <div class="item-wrap">
+	               	<img src="images/portfolio/liberty.jpg" alt="Liberty">
+	                  <a href="#modal-01" class="overlay">	                  	           
+	                     <div class="folio-item-table">
+	                     	<div class="folio-item-cell">
+		     					       <h3 class="folio-title">Liberty</h3>	     					    
+		     					    	 <span class="folio-types">
+		     					       	  Graphic Design
+		     					       </span>
+		     					   </div>	                      	
+	                     </div>                    
+	                  </a>
+	               </div>	               
+	        		</div> <!-- /folio-item -->
+
+      */
       for (let i in data) {
         const newCard = document.createElement("div");
+        newCard.setAttribute("class","bgrid folio-item");
+        const itemWrapper= document.createElement("div");
+        itemWrapper.setAttribute("class","item-wrapper");
+        const img = document.createElement("img");
+        img.src="images/portfolio/liberty.jpg" 
+        itemWrapper.appendChild(img)
+        newCard.appendChild(itemWrapper)
         var name = data[i].name;
         var newName = document.createElement("H3");
         newName.appendChild(document.createTextNode(name))
@@ -30,7 +54,7 @@ async function updateRepos() {
         newLang.appendChild(document.createTextNode(lang))
         newCard.appendChild(newLang);
 
-	var repoUrl = data[i].html_url;
+        var repoUrl = data[i].html_url;
         var newRepoUrl = document.createElement("a");
 
         var link = document.createTextNode("Check out this project!");
