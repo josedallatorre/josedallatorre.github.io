fb1e780202c98ba5753efe1a5ff08a0d697a4bab
josedallatorre <jose.dallatorre@hotmail.com>
2023-06-18 21:33:34 +0200

    changes

--- js/retrieve_repos.js
+++ js/retrieve_repos.js
@@ -1,66 +1,94 @@
-user = 'josedallatorre'
-apirepo = `https://api.github.com/users/${user}`
-//listrepos = document.createElement('column')
-const work = document.getElementById('works');
-var row = document.createElement("div")
-//row.classList.add("row folio-list block-lg-one-half block-stack-on-550")
-$.getJSON(apirepo + '/repos', function (data) {
-    console.log('data now', data)
+const url = "https://api.github.com/users/josedallatorre/repos?per_page=1ii";
 
-    function compare(a, b) {
-        if (a.watchers > b.watchers) {
-            return -1
-        }
-        if (a.watchers < b.watchers) {
-            return 1
-        }
-        return 0
+$.get(url, function(data) {
+
+  console.log(data);
+  for (let i in data) {
+  let sortedRepos = data.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
+  let repoName = sortedRepos[i].name;
+  let repoDescription = sortedRepos[i].description;
+  let repoLink = sortedRepos[i].html_url;
+  let repoStars = sortedRepos[i].stargazers_count;
+  let languages = [];
+  $.get(sortedRepos[i].languages_url, function(languageData) {
+    for(let i = 0; i < Object.keys(languageData).length; i++) {
+      languages.push(Object.keys(languageData)[i]);
     }
+  });
+  $(document).ready(function() {
+    for (let i in data){
+      var iDiv = document.createElement('div');
+      document.getElementsByTagName('container')[0].appendChild(iDiv);
+    $("#repoLink").attr('href', repoLink);
+    $("#repoTitle").html(repoName);
+    $("#repoStars").html(repoStars);
+    $("#repoDescription").html(repoDescription);
+    iDiv.appendChild(repoLink)
+    //New Code
+    let languageList = $("#repoLanguageList");
+    setTimeout(function() {
+      for(let i = 0; i < languages.length; i++) {
+        languageList.append(`<li><strong>${languages[i]}</strong></li>`);
+      }
+    });
+};
+  });
+};
+});
+async function updateRepos() {
+    const xhr = new XMLHttpRequest();
 
-    data.sort(compare)
-    data.forEach(v => {
-        //column
-        var new_col = document.createElement("div")
-        new_col.classList.add("column")
-        //new_col.classList.add("data-animate-el")
-        row.appendChild(new_col)
-            //folio-item div
-            folio_item = document.createElement('div')
-            folio_item.classList.add("folio-item")
-            new_col.appendChild(folio_item)
-                //folio-item__thumb
-                folio_item__thumb = document.createElement('div')
-                folio_item__thumb.classList.add("folio-item__thumb")
-                folio_item.appendChild(folio_item__thumb)
-                    //folio -item-link
-                    folio_item__thumb_link = document.createElement('a')
-                    folio_item__thumb.classList.add("folio-item__thumb-link")
-                    folio_item__thumb.appendChild(folio_item__thumb_link)
-                        //img
-                        //preview = document.createElement('img')
-                        //folio_item__thumb.classList.add("src")
-                        //folio_item__thumb.appendChild(folio_item__thumb_link)
-                        
+    const url = 'https://api.github.com/users/josedallatorre/repos';
 
-                //folio-item__info
-                    //folio-item__meta
-            
-                    //h4 folio-item__title
-            
-                //folio-item__caption
-                folio_item__caption = document.createElement('div')
-                folio_item__caption.classList.add("folio-item__caption")
-                caption = document.createElement('p')
-                caption.textContent = `${v.description} | Stars: ${v.watchers}`
-                folio_item__caption.appendChild(caption)
-                folio_item.appendChild(folio_item__caption)
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+    
+      // Parse API data into JSON
+      const data = JSON.parse(this.response);
+      
+      // Log the response
+      //console.log(data);
+      
+      // Loop over each object in data array
+      for (let i in data) {
+        var col = $('<div class="column" data-animate-el></div>');
+        var newCard = $('<div class="folio-item"></div>')
 
-        listItemRepo = document.createElement('div')
-        listItemRepo.classList.add("folio-item")
-        hlink = document.createElement('a')
-        listItemRepo.appendChild(hlink)
-        hlink.textContent = `${v.description} | Stars: ${v.watchers}`
-        hlink.href = v.html_url
-        //new_col.appendChild()
-    })
-})
+        var cardInfo = $('<div class="folio-item__info"></div>')
+        var name = data[i].name;
+        var newName = $('<div class="folio-item__title"></div>')
+        newName.appendChild(document.createTextNode(name))
+        cardInfo.append(newName)
+        newCard.append(cardInfo)
+        col.append(newCard)
+	    /* 
+        var descr = "Description:\n" + data[i].description;
+        var newDescr = document.createElement("p");
+        newDescr.appendChild(document.createTextNode(descr))
+        newCard.appendChild(newDescr);
+
+        var lang = "Language used:\n" + data[i].language;
+        var newLang = document.createElement("p");
+        newLang.appendChild(document.createTextNode(lang))
+        newCard.appendChild(newLang);
+
+	var repoUrl = data[i].html_url;
+        var newRepoUrl = document.createElement("a");
+
+        var link = document.createTextNode("Check out this project!");
+        newRepoUrl.appendChild(link);
+        newRepoUrl.title = "Repo link";
+        newRepoUrl.href = repoUrl;
+        newCard.appendChild(newRepoUrl);
+        */
+        $("#works").append(col);
+      }
+
+  }
+  
+  // Send the request to the server
+  xhr.send();
+  
+}
+
+updateRepos();
