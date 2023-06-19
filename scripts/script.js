91cdeb220df7abee6689395828b762ca63873653
josedallatorre <jose.dallatorre@hotmail.com>
2023-06-19 02:23:34 +0200

    modifying repo sectiion

--- scripts/script.js
+++ scripts/script.js
@@ -20,16 +20,48 @@ async function updateRepos() {
         itemWrapper.setAttribute("class","item-wrapper");
         const img = document.createElement("img");
         img.src="images/portfolio/liberty.jpg" 
-
-        itemWrapper.appendChild(img)
-        newCard.appendChild(itemWrapper)
+        var modalUrl = document.createElement("a");
+        modalUrl.setAttribute("class", "overlay");
+        modalUrl.href= "#modal-"+ i
+        const folioTable = document.createElement("div");
+        folioTable.setAttribute("class","folio-item-table");
+        const folioCell = document.createElement("div");
+        folioCell.setAttribute("class","folio-item-cell");
+        
         var name = data[i].name;
         var newName = document.createElement("H3");
-        //var modUrlal = document.createElement("a");
-        //var modallink = 
-        newName.appendChild(document.createTextNode(name))
-        newCard.appendChild(newName);
-	      
+        newName.appendChild(document.createTextNode(name));
+        newName.setAttribute("class","folio-title");
+        var newType = document.createElement("span");
+        newType.setAttribute("class","folio-types");
+        newType.appendChild(document.createTextNode("Development"));
+        folioCell.appendChild(newName);
+        folioCell.appendChild(newType);
+        folioTable.appendChild(folioCell)
+        itemWrapper.appendChild(img)
+        itemWrapper.appendChild(modalUrl)
+        itemWrapper.appendChild(folioTable)
+        newCard.appendChild(itemWrapper)
+        
+
+        //TODO fare come https://mayanigrin.com/ nei progetti, no modal
+        var newModal = document.createElement("div");
+        newModal.setAttribute("id","modal-"+i);
+        newModal.setAttribute("class","popup-modal slider ");
+        const newMedia = document.createElement("div");
+        newMedia.setAttribute("class","media");
+        const imgMedia = document.createElement("img");
+        imgMedia.src="images/portfolio/modals/m-shutterbug.jpg"
+        var descrBox = document.createElement("div");
+        descrBox.setAttribute("class","description-box");
+        var descName = document.createElement("H4");
+        descName.appendChild(document.createTextNode(name));
+        descrBox.appendChild(descName)
+        newMedia.appendChild(imgMedia)
+        newModal.appendChild(newMedia)
+        newModal.appendChild(descrBox)
+
+        /*
         var descr = data[i].description;
         var newDescr = document.createElement("p");
         newDescr.appendChild(document.createTextNode(descr))
@@ -48,8 +80,10 @@ async function updateRepos() {
         newRepoUrl.title = "Repo link";
         newRepoUrl.href = repoUrl;
         newCard.appendChild(newRepoUrl);
+        */
 
         document.getElementById("repos-container").appendChild(newCard);
+        document.getElementById("modal-container").appendChild(newModal);
       }
 
   }
