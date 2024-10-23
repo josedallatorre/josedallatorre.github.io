09cb21f9bc033c3f917b451ec8866e319e876284
josedallatorre <jose.dallatorre@hotmail.com>
2024-10-23 20:22:04 +0100

    started from scratch

--- app/components/aboutme.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <p>I am a passionate and dedicated Computer Science student with a strong interest in solving complex problems through innovative technology solutions. My goal is to make a positive impact on the world by developing cutting-edge software applications and pushing the boundaries of what is possible in the digital realm.</p>;
-  }
--- app/resume/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <p>qua sono su resume</p>;
-  }
