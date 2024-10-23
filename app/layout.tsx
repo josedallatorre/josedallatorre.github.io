e202f2d59ec483ae210f6eaf9b9e3422ccd3954a
josedallatorre <jose.dallatorre@hotmail.com>
2024-10-23 20:31:31 +0100

    Revert "checkpoint, switching to a new branch"
    This reverts commit dd11d71db88a842d415d909f22f5d6c68a71f89f.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import './global.css'
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { baseUrl } from './sitemap'
+import SocialLinks from 'app/components/social'
+import Header from 'app/components/header'
+import Footer from 'app/components/footer'
+import { Navbar } from './components/nav';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +21,53 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en"
+    className={cx(
+      'text-black bg-white dark:text-white dark:bg-black',
+      GeistSans.variable,
+      GeistMono.variable
+    )}>
+      <body>
+    <div className="min-h-screen  text-white">
+    {/* Mobile Layout */}
+    <div className="lg:hidden">
+      <main className="p-6 max-w-screen-sm mx-auto">
+        <div className="mb-6">
+          {/*<Header />*/}
+          <div className="mt-6">
+            {/*<SocialLinks/>*/}
+          </div>
+        </div>
+      </main>
+      {/*<Footer />*/}
+    </div>
+
+    {/* Desktop Layout */}
+    <div className="hidden lg:flex flex-col xl:flex-row">
+      {/* Left Column (Header and Social Links) */}
+      <div className="xl:fixed xl:w-2/5 h-screen p-8 xl:p-12 xl:pl-[10%] flex flex-col 
+      justify-between min-w-[300px] max-w-[800px]  ">
+        <div>
+          <Header />
+          <div className="mt-6">
+            <SocialLinks />
+          </div>
+        </div>
+      </div>
+      {/* Right Column (Main Content) */}
+      <div className="xl:ml-[40%] w-full xl:w-3/5 min-h-screen min-w-[500px] max-w-[1200px] mx-auto">
+        <main className="p-8 xl:p-12 xl:pr-[10%]">
+          <Navbar/>
+          <div className="mt-3"> 
+            {children}
+            <Footer />
+            <Analytics />
+            <SpeedInsights /> 
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
       </body>
     </html>
   );
