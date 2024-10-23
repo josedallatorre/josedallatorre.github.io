4e0aea705088becde190b31616cce68de1f3b5c7
josedallatorre <jose.dallatorre@hotmail.com>
2024-10-23 03:07:41 +0100

    feat: copied readme from previous version

---
+++ app/components/aboutme.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>I am a passionate and dedicated Computer Science student with a strong interest in solving complex problems through innovative technology solutions. My goal is to make a positive impact on the world by developing cutting-edge software applications and pushing the boundaries of what is possible in the digital realm.</p>;
+  }
