2c71acf1f2a7fa4f8ae807d0aa0d9b561adf78cc
josedallatorre <jose.dallatorre@hotmail.com>
2024-10-24 23:34:54 +0100

    style: 1st version with same style

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,11 +5,13 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <section id="resume">
-      <h1 className="fw-bold">More of my credentials.</h1>
-      <h3>Here are my work experiences and education.</h3>
-      <h2 className="mt-6 mb-4">Work Experience</h2>
-      <MdWork />
-      <div className="border-l border-sky-500">
+      <p className="fw-bold">More of my credentials.</p>
+      <p className="text-3xl font-bold">Here are my work experiences and education.</p>
+      <div className="mt-6 mb-4 flex flex-row">
+        <MdWork />
+        <p className="mx-2">Work Experience</p>
+      </div>
+      <div className="border-l border-zinc-400 mt-8">
         <div className="ml-4 flex flex-row">
           <div className="basis-1/4">
             <Image
@@ -21,23 +23,29 @@ export default function Page() {
             />
           </div>
           <div className="basis-3/4">
-          <h3>IoT Developer - Intern</h3>
-          <h4>ADL Ingegneria Informatica</h4>
-          <p>April 2022 - June 2022</p>
+            <p className="text-3xl font-bold">IoT Developer - Intern</p>
+            <p className="text-lg text-zinc-200 font-bold">ADL Ingegneria Informatica</p>
+            <p className="text-zinc-400 font-bold">April 2022 - June 2022</p>
           </div>
         </div>
-        <div className="ml-4">
-          <p>Designed and developed a IoT system capable of handling RFID based communication.
+        <div className="ml-6 mt-4">
+          <ul className="list-disc">
+            <li className="text-slate-100 font-bold">Designed and developed a IoT system capable of handling RFID based communication.
             Integrated with the architecture used by the company.
-          </p>
+            </li>
+            <li className="text-slate-100 font-bold">Integrated with the architecture used by the company.
+            </li>
+          </ul>
         </div>
       </div>
 
 
-      <h2 className="mt-6 mb-4">Education</h2>
+      <div className="mt-6 mb-4 flex flex-row">
+        <FaGraduationCap />
+        <p className="mx-2">Education</p>
+      </div>
 
-      <FaGraduationCap />
-      <div className="border-l border-sky-500">
+      <div className="border-l border-zinc-400 mt-8">
         <div className="ml-4 flex flex-row">
           <div className="basis-1/4">
             <Image
@@ -49,18 +57,17 @@ export default function Page() {
             />
           </div>
           <div className="basis-3/4">
-            <h3>Master Degree</h3>
-            <h4>March 2023 - Present</h4>
-            <p>University of Udine</p>
+            <p className="text-3xl font-bold">Master Degree</p>
+            <p className="text-lg text-zinc-200 font-bold">University of Udine</p>
+            <p className="text-zinc-400 font-bold">March 2023 - Present</p>
           </div>
         </div>
-        <div className="ml-4">
-          <p>Relevant coursework: Software Engineering 2, Advanced object-oriented design and analysis, Recommender Systems, AI</p>
+        <div className="ml-4 mt-4">
+          <p className="text-slate-100 font-bold">Relevant coursework: Software Engineering 2, Advanced object-oriented design and analysis, Recommender Systems, AI</p>
         </div>
       </div>
 
-      <FaGraduationCap />
-      <div className="border-l border-sky-500">
+      <div className="border-l border-zinc-400 mt-8">
         <div className="ml-4 flex flex-row">
           <div className="basis-1/4">
             <Image
@@ -72,19 +79,18 @@ export default function Page() {
             />
           </div>
           <div className="basis-3/4">
-            <h3>Bachelor Degree</h3>
-            <h4>September 2019 - March 2023</h4>
-            <p>University of Udine</p>
+            <p className="text-3xl font-bold">Bachelor Degree</p>
+            <p className="text-lg text-zinc-200 font-bold">University of Udine</p>
+            <p className="text-zinc-400 font-bold">September 2019 - March 2023</p>
           </div>
         </div>
-        <div className="ml-4">
-          <p>Relevant coursework: Software Engineering, Computer Networks, Database Systems, User Interface Design, Algorithms and Data Structures</p>
+        <div className="ml-4 mt-4">
+          <p className="text-slate-100 font-bold">Relevant coursework: Software Engineering, Computer Networks, Database Systems, User Interface Design, Algorithms and Data Structures</p>
         </div>
       </div>
 
 
-      <FaGraduationCap />
-      <div className="border-l border-sky-500">
+      <div className="border-l border-zinc-400 mt-8">
         <div className="ml-4 flex flex-row">
           <div className="basis-1/4">
             <Image
@@ -96,13 +102,13 @@ export default function Page() {
             />
           </div>
           <div className="basis-3/4">
-            <h3>High School Diploma</h3>
-            <h4>September 2013 - July 2019</h4>
-            <p>ISIS Giuseppe Marchesini</p>
+            <p className="text-3xl font-bold">High School Diploma</p>
+            <p className="text-lg text-zinc-200 font-bold">ISIS Giuseppe Marchesini</p>
+            <p className="text-zinc-400 font-bold">September 2013 - July 2019</p>
           </div>
         </div>
-        <div className="ml-4">
-          <p>I partecipated in projects with IT interest like AR development and economics and entrepreneurship focus.
+        <div className="ml-4 mt-4">
+          <p className="text-slate-100 font-bold">I partecipated in projects with IT interest like AR development and economics and entrepreneurship focus.
             Received extra curricula credit as a student athlete.
           </p>
         </div>
