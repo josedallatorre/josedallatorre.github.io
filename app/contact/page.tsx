0d87ad7031156a353f6d436b6482a678b2538fed
josedallatorre <jose.dallatorre@hotmail.com>
2024-10-25 23:40:40 +0100

    feat: added form to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,9 @@
+import Contact from '@/components/contact';
+
 export default function Page() {
-  return (<p>contact page</p>);
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-white'>
+      <Contact />
+    </main>
+  )
 }
