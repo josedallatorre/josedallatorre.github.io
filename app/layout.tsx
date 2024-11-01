f3b177c6eae82a8d1fb54dd4b2c53e416a3e2e2e
josedallatorre <jose.dallatorre@hotmail.com>
2024-11-01 20:28:59 +0000

    better indentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,52 +47,52 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en"
-    className={cx(
-      'text-black bg-white dark:text-white dark:bg-black',
-      GeistSans.variable,
-      GeistMono.variable
-    )}>
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+        GeistSans.variable,
+        GeistMono.variable
+      )}>
       <body>
-    <div className="min-h-screen  text-white">
-    {/* Mobile Layout */}
-    <div className="lg:hidden">
-      <main className="p-6 max-w-screen-sm mx-auto">
-        <div className="mb-6">
-          {/*<Header />*/}
-          <div className="mt-6">
-            {/*<SocialLinks/>*/}
+        <div className="min-h-screen  text-white">
+          {/* Mobile Layout */}
+          <div className="lg:hidden">
+            <main className="p-6 max-w-screen-sm mx-auto">
+              <div className="mb-6">
+                {/*<Header />*/}
+                <div className="mt-6">
+                  {/*<SocialLinks/>*/}
+                </div>
+              </div>
+            </main>
+            {/*<Footer />*/}
           </div>
-        </div>
-      </main>
-      {/*<Footer />*/}
-    </div>
 
-    {/* Desktop Layout */}
-    <div className="hidden lg:flex flex-col xl:flex-row">
-      {/* Left Column (Header and Social Links) */}
-      <div className="xl:fixed xl:w-2/5 h-screen p-8 xl:p-12 xl:pl-[10%] flex flex-col 
+          {/* Desktop Layout */}
+          <div className="hidden lg:flex flex-col xl:flex-row">
+            {/* Left Column (Header and Social Links) */}
+            <div className="xl:fixed xl:w-2/5 h-screen p-8 xl:p-12 xl:pl-[10%] flex flex-col 
       justify-between min-w-[300px] max-w-[800px]  ">
-        <div>
-          <Header />
-          <div className="mt-6">
-            <SocialLinks />
+              <div>
+                <Header />
+                <div className="mt-6">
+                  <SocialLinks />
+                </div>
+              </div>
+            </div>
+            {/* Right Column (Main Content) */}
+            <div className="xl:ml-[40%] w-full xl:w-3/5 min-h-screen min-w-[500px] max-w-[1200px] mx-auto">
+              <main className="p-8 xl:p-12 xl:pr-[10%]">
+                <Navbar />
+                <div className="mt-3">
+                  {children}
+                  <Footer />
+                  <Analytics />
+                  <SpeedInsights />
+                </div>
+              </main>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Right Column (Main Content) */}
-      <div className="xl:ml-[40%] w-full xl:w-3/5 min-h-screen min-w-[500px] max-w-[1200px] mx-auto">
-        <main className="p-8 xl:p-12 xl:pr-[10%]">
-          <Navbar/>
-          <div className="mt-3"> 
-            {children}
-            <Footer />
-            <Analytics />
-            <SpeedInsights /> 
-          </div>
-        </main>
-      </div>
-    </div>
-  </div>
       </body>
     </html>
   )
